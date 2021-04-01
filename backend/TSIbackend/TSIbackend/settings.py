9dd843994e1a4a03faec5f46985dba981a0ac562
xPhelippe <42011270+xPhelippe@users.noreply.github.com>
2021-04-01 12:06:44 -0400

    added database connection to django project

--- backend/TSIbackend/TSIbackend/settings.py
+++ backend/TSIbackend/TSIbackend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'TSIbackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'skilesindex',
+        'USER': 'skiles_user',
+        'PASSWORD': 'theskilesindex',
+        'HOST': 'db.joanneskiles.com',
+        'PORT': '5432',
     }
 }
 
