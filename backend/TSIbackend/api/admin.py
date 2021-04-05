b8363cd465a5262ae47e24cf8ffa25401c7524d0
xPhelippe <42011270+xPhelippe@users.noreply.github.com>
2021-04-04 23:20:35 -0400

    Register models to the admin site

--- backend/TSIbackend/api/admin.py
+++ backend/TSIbackend/api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+admin.site.register(Stock)
+admin.site.register(Profile)
+admin.site.register(StockDailyData)
+admin.site.register(StockRSIData)
+admin.site.register(StockSMAData)
+admin.site.register(StockVWAPData)
+admin.site.register(FavStock)
+
+
