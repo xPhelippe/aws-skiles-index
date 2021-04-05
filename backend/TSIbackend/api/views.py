a86a9b76c6aa44a420e8501c07e432c3f8260bb3
xPhelippe <42011270+xPhelippe@users.noreply.github.com>
2021-04-04 21:40:40 -0400

    make basic login method

--- backend/TSIbackend/api/views.py
+++ backend/TSIbackend/api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+
+@csrf_exempt
+@require_http_methods(['POST']) 
+def login_user(request):
+
+    # get the username of the user
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+
+    # try to get the user
+    try:
+        user = User.objects.get(username=username)
+    except: 
+        return HttpResponse("<p> user DNE <p>")
+
+    # see if the password is correct
+    if user.check_password(password):
+        
+        # grab user information and send over
+        return HttpResponse("<p> successful Login"
+    else:
+        return HttpResponse("<p>bad password<p>")
+
+    
