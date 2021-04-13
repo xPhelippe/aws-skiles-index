0f230b1525d3674b67ed16954f0f049bf49d2860
Mihir Lad <mihirlad55@gmail.com>
2021-04-13 02:05:02 -0400

    Get REACT_APP_API_HOST from environment variable
    This is used for production to get the API host address, so localhost is only
    used in development.

---
+++ frontend/src/components/Environment.js
@@ -0,0 +1,8 @@
+export const getAPIHost = () => {
+  if (process.env.REACT_APP_API_HOST !== null)
+    return process.env.REACT_APP_API_HOST;
+  else
+    return 'http://127.0.0.1:8000';
+}
+
+export default getAPIHost;
