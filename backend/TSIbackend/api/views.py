6cb9ffe4becf7d71c49b9bb64c927251c9c2c7fa
Mihir Lad <mihirlad55@gmail.com>
2021-04-16 00:46:43 -0400

    Allow update_stock_data to update new data
    Previously, update_stock_data could only populate an initially empty database.
    With these modifications, update_stock_data only adds new data, rather than
    failing when trying to add pre-existing data to the database.
    
    The update_stock_data function is split into multiple functions to make it more
    modular. The stock overview data, daily adjusted data, and each technical
    indicator is updated for the stock in a separate function.
    
    Each of these functions follow a similar pattern of finding the latest
    data entry and only adding data to the database with a timestamp that proceeds
    the latest data entry's timestamp.

--- backend/TSIbackend/api/views.py
+++ backend/TSIbackend/api/views.py
@@ -1,5 +1,7 @@
 from time import sleep
+from datetime import datetime
 
+from django.utils import timezone
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods, require_GET
@@ -450,92 +452,185 @@ def login_user(request):
         return JsonResponse(resp,status=200)
 
 
-def update_stock_data(request):
-    # Default options to use
-    DEFAULT_SERIES_TYPE = 'close'
-    DEFAULT_INTERVAL = 'daily'
-    DEFAULT_OUTPUT_SIZE = 'compact'
-
-    # Initialize alpha vantage API objects
-    ts = TimeSeries(key=ALPHA_VANTAGE_API_KEY,
-                    output_format='json', indexing_type='date')
+def update_stock_overview(ticker):
     fd = FundamentalData(key=ALPHA_VANTAGE_API_KEY,
-                         output_format='json', indexing_type='date')
-    ti = TechIndicators(key=ALPHA_VANTAGE_API_KEY,
-                        output_format='json', indexing_type='date')
+            output_format='json', indexing_type='date')
 
+    print("Getting company overview")
+    company_data, metadata = fd.get_company_overview(ticker)
+
+    print("Updating company overview")
+    try:
+        stock = Stock.objects.get(ticker=ticker)
+        print("Stock already in database. Not adding.")
+    except ObjectDoesNotExist:
+        print("Adding stock to database")
+        stock = Stock()
+        stock.ticker = ticker
+
+    stock.description = company_data['Description'][:3000]
+    stock.fullName = company_data['Name'][:50]
+    stock.save()
+
+    return stock
+
+def update_stock_daily_data(stock):
+    ts = TimeSeries(key=ALPHA_VANTAGE_API_KEY,
+            output_format='json', indexing_type='date')
+
+    print("Getting ticker daily adjusted")
+    daily_data, metadata = ts.get_daily_adjusted(
+            symbol=stock.ticker, outputsize='compact')
+
+    try:
+        latest_data = StockDailyData.objects.filter(stock=stock).latest('timestamp')
+        print("Existing daily adjusted data found in database")
+        latest_timestamp = latest_data.timestamp
+    except ObjectDoesNotExist:
+        latest_timestamp = None
+        print("No daily adjusted data found in database")
+
+    print(f"Updating stock daily data ({len(daily_data)} entries to process)")
+    daily_data_set = []
+    for day in daily_data:
+        # Don't add existing data
+        timestamp = timezone.make_aware(datetime.strptime(day, '%Y-%m-%d'), timezone.get_default_timezone())
+        if latest_timestamp != None and timestamp < latest_timestamp:
+            continue
+
+        db_daily_data = StockDailyData()
+        db_daily_data.stock = stock
+        db_daily_data.timestamp = timestamp
+        db_daily_data.interval = 'daily'
+        db_daily_data.open = daily_data[day]['1. open']
+        db_daily_data.high = daily_data[day]['2. high']
+        db_daily_data.low = daily_data[day]['3. low']
+        db_daily_data.close = daily_data[day]['4. close']
+        db_daily_data.save()
+
+        daily_data_set.append(db_daily_data)
+
+    return daily_data_set
+
+
+def update_sma_data(stock):
+    ti = TechIndicators(key=ALPHA_VANTAGE_API_KEY,
+            output_format='json', indexing_type='date')
+
+    print("Getting SMA data")
+    sma_data, metadata = ti.get_sma(symbol=stock.ticker, interval='daily',
+            series_type='compact')
+
+    try:
+        latest_data = StockSMAData.objects.filter(stock=stock).latest('timestamp')
+        print("Existing SMA data found in database")
+        latest_timestamp = latest_data.timestamp
+    except ObjectDoesNotExist:
+        latest_timestamp = None
+        print("No SMA data found in database")
+
+    print(f"Updating stock SMA data ({len(sma_data)} entries to process)")
+    sma_data_set = []
+    for time in sma_data:
+        timestamp = timezone.make_aware(datetime.strptime(time, '%Y-%m-%d'), timezone.get_default_timezone())
+        # Don't add existing data
+        if latest_timestamp != None and timestamp < latest_timestamp:
+            continue
+
+        db_sma_data = StockSMAData()
+        db_sma_data.stock = stock
+        db_sma_data.timestamp = timestamp
+        db_sma_data.SMA = sma_data[time]['SMA']
+        db_sma_data.save()
+
+        sma_data_set.append(db_sma_data)
+
+    return sma_data_set
+
+
+def update_vwap_data(stock):
+    ti = TechIndicators(key=ALPHA_VANTAGE_API_KEY,
+            output_format='json', indexing_type='date')
+
+    print("Getting VWAP data")
+    vwap_data, metadata = ti.get_vwap(symbol=stock.ticker, interval='5min')
+
+    try:
+        latest_data = StockVWAPData.objects.filter(stock=stock).latest('timestamp')
+        print("Existing VWAP data found in database")
+        latest_timestamp = latest_data.timestamp
+    except ObjectDoesNotExist:
+        latest_timestamp = None
+        print("No VWAP data found in database")
+
+    print(f"Updating stock VWAP data ({len(vwap_data)} entries to process)")
+    vwap_data_set = []
+    for time in vwap_data:
+        timestamp = timezone.make_aware(datetime.strptime(time, '%Y-%m-%d %H:%M'), timezone.get_default_timezone())
+        # Don't add existing data
+        if latest_timestamp != None and timestamp < latest_timestamp:
+            continue
+
+        db_vwap_data = StockVWAPData()
+        db_vwap_data.stock = stock
+        db_vwap_data.timestamp = timestamp
+        db_vwap_data.VWAP = vwap_data[time]['VWAP']
+        db_vwap_data.save()
+
+        vwap_data_set.append(db_vwap_data)
+
+    return vwap_data_set
+
+
+def update_rsi_data(stock):
+    ti = TechIndicators(key=ALPHA_VANTAGE_API_KEY,
+            output_format='json', indexing_type='date')
+
+    print("Getting RSI data")
+    rsi_data, metadata = ti.get_rsi(symbol=stock.ticker, interval='daily',
+            series_type='close')
+
+    try:
+        latest_data = StockRSIData.objects.filter(stock=stock).latest('timestamp')
+        print("Existing RSI data found in database")
+        latest_timestamp = latest_data.timestamp
+    except ObjectDoesNotExist:
+        latest_timestamp = None
+        print("No RSI data found in database")
+
+    print(f"Updating stock RSI data ({len(rsi_data)} entries to process)")
+    rsi_data_set = []
+    for time in rsi_data:
+        timestamp = timezone.make_aware(datetime.strptime(time, '%Y-%m-%d'), timezone.get_default_timezone())
+        # Don't add existing data
+        if latest_timestamp != None and timestamp < latest_timestamp:
+            continue
+
+        db_rsi_data = StockRSIData()
+        db_rsi_data.stock = stock
+        db_rsi_data.timestamp = timestamp
+        db_rsi_data.RSI = rsi_data[time]['RSI']
+        db_rsi_data.save()
+
+        rsi_data_set.append(db_rsi_data)
+
+    return rsi_data_set
+
+
+def update_stock_data(request):
     # Iterate through every stock in settings.py
     for ticker in SELECTED_STOCK_TICKERS:
         # Avoid API limit (5/min)
         print("Waiting 60 seconds to avoid reaching API limit")
         sleep(60)
 
-        print(f"Updating ticker {ticker}")
-        print(
-            f"  Getting ticker daily adjusted with output size {DEFAULT_OUTPUT_SIZE}")
-        daily_data, metadata = ts.get_daily_adjusted(
-            symbol=ticker, outputsize=DEFAULT_OUTPUT_SIZE)
-
-        print("  Getting company overview")
-        company_data, metadata = fd.get_company_overview(ticker)
-
-        print(
-            f"  Getting SMA with interval {DEFAULT_INTERVAL} and series type {DEFAULT_SERIES_TYPE}")
-        sma_data, metadata = ti.get_sma(symbol=ticker, interval=DEFAULT_INTERVAL,
-                                        series_type=DEFAULT_SERIES_TYPE)
-
-        print("  Getting VWAP with interval 5min")
-        vwap_data, metadata = ti.get_vwap(symbol=ticker, interval='5min')
-
-        print(
-            f"  Getting RSI with interval {DEFAULT_INTERVAL} and series type {DEFAULT_SERIES_TYPE}")
-        rsi_data, metadata = ti.get_rsi(symbol=ticker, interval=DEFAULT_INTERVAL,
-                                        series_type=DEFAULT_SERIES_TYPE)
-
-        print("  Updating stock details")
-        stock = Stock()
-        stock.ticker = ticker
-        stock.description = company_data['Description'][:3000]
-        stock.fullName = company_data['Name'][:50]
-        stock.save()
-
-        print("  Updating stock data")
-
-        print("    Updating stock daily data")
-        for day in daily_data:
-            db_daily_data = StockDailyData()
-            db_daily_data.stock = stock
-            db_daily_data.timestamp = day
-            db_daily_data.interval = DEFAULT_INTERVAL
-            db_daily_data.open = daily_data[day]['1. open']
-            db_daily_data.high = daily_data[day]['2. high']
-            db_daily_data.low = daily_data[day]['3. low']
-            db_daily_data.close = daily_data[day]['4. close']
-            db_daily_data.save()
-
-        print("    Updating stock SMA data")
-        for time in sma_data:
-            db_sma_data = StockSMAData()
-            db_sma_data.stock = stock
-            db_sma_data.timestamp = time
-            db_sma_data.SMA = sma_data[time]['SMA']
-            db_sma_data.save()
-
-        print("    Updating stock VWAP data")
-        for time in vwap_data:
-            db_vwap_data = StockVWAPData()
-            db_vwap_data.stock = stock
-            db_vwap_data.timestamp = time
-            db_vwap_data.VWAP = vwap_data[time]['VWAP']
-            db_vwap_data.save()
-
-        print("    Updating stock RSI data")
-        for time in rsi_data:
-            db_rsi_data = StockRSIData()
-            db_rsi_data.stock = stock
-            db_rsi_data.timestamp = time
-            db_rsi_data.RSI = rsi_data[time]['RSI']
-            db_rsi_data.save()
+        print(f"--------Updating stock {ticker}--------")
+        stock = update_stock_overview(ticker)
+        update_stock_daily_data(stock)
+        update_sma_data(stock)
+        update_vwap_data(stock)
+        update_rsi_data(stock)
+        print("")
 
     return JsonResponse({"status":"Done updating stock data"})
 
