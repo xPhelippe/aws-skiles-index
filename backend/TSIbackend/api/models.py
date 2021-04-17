6294ecb8fb2962d5ea152e6723057466a80c8389
Elisa <rexingere@gmail.com>
2021-04-17 14:13:19 -0400

    Pulling stock ticker endpoint

--- backend/TSIbackend/api/models.py
+++ backend/TSIbackend/api/models.py
@@ -8,7 +8,7 @@ from django.dispatch import receiver
 
 # holds generic infomration about stock
 class Stock(models.Model):
-    ticker = models.CharField(max_length=10,unique=True)
+    ticker = models.CharField(max_length=10, unique=True)
     description = models.CharField(max_length=3000)
     fullName = models.CharField(max_length=50)
 
@@ -16,41 +16,49 @@ class Stock(models.Model):
         return self.ticker
 
 # holds time based data about stock
+
+
 class StockDailyData(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now=False,auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
     interval = models.CharField(max_length=10)
-    open = models.DecimalField(max_digits=7,decimal_places=2)
-    high = models.DecimalField(max_digits=7,decimal_places=2)
-    low = models.DecimalField(max_digits=7,decimal_places=2)
-    close = models.DecimalField(max_digits=7,decimal_places=2)
+    open = models.DecimalField(max_digits=7, decimal_places=2)
+    high = models.DecimalField(max_digits=7, decimal_places=2)
+    low = models.DecimalField(max_digits=7, decimal_places=2)
+    close = models.DecimalField(max_digits=7, decimal_places=2)
+
 
 class StockSMAData(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now=False,auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
     interval = models.CharField(max_length=10)
-    SMA = models.DecimalField(max_digits=7,decimal_places=2)
+    SMA = models.DecimalField(max_digits=7, decimal_places=2)
+
 
 class StockVWAPData(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now=False,auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
     interval = models.CharField(max_length=10)
-    VWAP = models.DecimalField(max_digits=7,decimal_places=2)
+    VWAP = models.DecimalField(max_digits=7, decimal_places=2)
+
 
 class StockRSIData(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now=False,auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
     interval = models.CharField(max_length=10)
-    RSI = models.DecimalField(max_digits=7,decimal_places=2)
+    RSI = models.DecimalField(max_digits=7, decimal_places=2)
 
 # profile to hold user phone number and their investment type
+
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    phoneNumber = models.CharField(max_length=12,blank=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
+    phoneNumber = models.CharField(max_length=12, blank=True)
 
     investmentType = (
-        (0,"LOWRISK"),
-        (1,"HIGHRISK"),
+        (0, "LOWRISK"),
+        (1, "HIGHRISK"),
     )
 
     investmentType = models.IntegerField(choices=investmentType, default=0)
@@ -59,10 +67,13 @@ class Profile(models.Model):
         return str(self.user)
 
 # code to update the user profile whenerver the user is editted
+
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
@@ -70,25 +81,24 @@ def save_user_profile(sender, instance, **kwargs):
 
 
 class FavStock(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='watchlist')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='watchlist')
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.user) + ', ' + str(self.stock)
-    
 
 
 class StockOverview(models.Model):
 
-    stock = models.ForeignKey(Stock, on_delete=models.CASCADE,related_name="overview") 
+    stock = models.ForeignKey(
+        Stock, on_delete=models.CASCADE, related_name="overview")
 
-    PriceToBookRatio = models.DecimalField(max_digits=7,decimal_places=2)
-    PERatio = models.DecimalField(max_digits=7,decimal_places=2)
-    PEGRatio = models.DecimalField(max_digits=7,decimal_places=2)
-    PriceToSalesRatioTTM = models.DecimalField(max_digits=7,decimal_places=2)
-    ShortRatio = models.DecimalField(max_digits=7,decimal_places=2)
+    PriceToBookRatio = models.DecimalField(max_digits=7, decimal_places=2)
+    PERatio = models.DecimalField(max_digits=7, decimal_places=2)
+    PEGRatio = models.DecimalField(max_digits=7, decimal_places=2)
+    PriceToSalesRatioTTM = models.DecimalField(max_digits=7, decimal_places=2)
+    ShortRatio = models.DecimalField(max_digits=7, decimal_places=2)
 
     def __str__(self):
         return str(self.stock)
-
-
